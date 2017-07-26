8d36f97b1646214956821495ab32c587cd867c36
Earth <earth_nut_harry@hotmail.com>
2017-07-26 17:01:27 +0700

    add column provider_key, provider_secret to Provider.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705094030) do
+ActiveRecord::Schema.define(version: 20170726080316) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "namespace"
@@ -45,8 +45,10 @@ ActiveRecord::Schema.define(version: 20170705094030) do
 
   create_table "providers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "provider_key"
+    t.string   "provider_secret"
   end
 
   create_table "stickers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
